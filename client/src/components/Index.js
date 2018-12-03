e1b6e1812c764cdd8f459106d6e596b3c4a46b1f
Joe Lewis <lewis.joe.18@gmail.com>
2018-12-03 22:28:58 +0530

    fixed navbar bug + more fixes

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -2,12 +2,16 @@ import React, {Component} from "react";
 import { Layout, Menu, Breadcrumb, Icon, Dropdown } from 'antd';
 import { Row, Col } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 import Landing from './Landing.js'
+import PostList from './PostList.js'
+import HomeMenu from './HomeMenu.js'
 import Model from '../models'
+import {createBrowserHistory} from 'history';
+import $ from 'jquery';
 
 
-var PostList = () => <div> post list </div>
+// var PostList = () => <div> post list </div>
 var DraftList = () => <div> draft list </div>
 var PostView = () => <div> post view </div>
 var PostEdit = () => <div> post edit </div>
@@ -32,31 +36,42 @@ const UserMenu = (
     </Menu>
 );
 
+
+
 class Index extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedKey: ''
+        }
+    }
+
+    setSelectedKey(key) {
+        console.log(key);
+        this.setState({selectedKey: key});
+    }
+
+    onSelect(item) {
+        this.setState({selectedKey: item.key})
+    }
+
+    componentDidUpdate() {
+    }
+
     render(){
       return(
+        <Route path="/">
         <Layout className="layout">
             <Header className="writango-header">
-            <div className="writango-logo"><Link style={{ textDecoration: 'none',  color: '#555'}} to="/"> <span className="font-color-blue">W</span>RITANG<span className="font-color-blue">O</span></Link></div>
+            <div onClick={() => { $(window).trigger('RefreshMenu') }} className="writango-logo"><Link style={{ textDecoration: 'none',  color: '#555'}} to="/"> <span className="font-color-blue">W</span>RITANG<span className="font-color-blue">O</span></Link></div>
             <Dropdown overlay={Model.session.user.anonymous ? AnonymousMenu : UserMenu} trigger={['click']}>
                 <a style={{float: "right"}} className="ant-dropdown-link" href="#">
                     {Model.session.user.anonymous ? "Hello, Mr. Anonymous!" : Model.session.user.email} <Icon type="down" />
                 </a>
             </Dropdown>
-            <Menu
-                // theme="dark"
-                mode="horizontal"
-                // defaultSelectedKeys={['2']}
-                style={{ lineHeight: '64px' }}
-                className="writango-menu-container"
-            >
-                <Menu.Item key="1"><Link to={"/writes/posts"}>Posts</Link></Menu.Item>
-                <Menu.Item key="2">Drafts</Menu.Item>
-                <Menu.Item key="3">Write</Menu.Item>
-                {/* <Menu.Item key="3">nav 3</Menu.Item> */}
-            </Menu>
+            <HomeMenu selectedKey={this.state.selectedKey} onSelect={this.onSelect.bind(this)}/>
             </Header>
-            <Content style={{ padding: '0 50px', background: '#fff' }}>
+            <Content style={{ padding: '50px 50px', background: '#fff' }}>
                     <div>
                     <Route path="/" exact component={Landing} />
                     <Route path="/writes/:username" exact component={PostList} />
@@ -69,6 +84,7 @@ class Index extends Component{
                 Just another weekend project created with the awesome <a href="https://github.com/prosemirror">Prosemirror</a> library & other open source technologies. Follow me <a href="https://twitter.com/@vettijoe">@vettijoe</a>, <a href="https://github.com/joelewis">github.com/joelewis</a> and <a href="https://hexopress.com/@joe">hexopress.com/@joe</a>.
             </Footer>
         </Layout>
+        </Route>
       );
     }
 }
